375012b45587e159b1359407a53c1a0bc7ee1902
yuval-ro <68990745+yuval-ro@users.noreply.github.com>
2022-01-10 21:22:39 +0200

    Monday

--- Element.cpp
+++ Element.cpp
@@ -1,5 +1,56 @@
-//
-// Created by omril on 01/10/2022.
-//
-
 #include "Element.h"
+
+Element::Element() {
+    x = NULL;
+    t = NULL;
+    x_size = 0;
+    t_size = 0;
+    rank = 0;
+    flag = false;
+}
+Element::Element(double* x, double* t, int x_size, int t_size) {
+    this->x = x;
+    this->t = t;
+    this->x_size = x_size;
+    this->t_size = t_size;
+    rank = 0;
+    flag = false;
+}
+void Element::updateRank(int r) {
+    rank = r;
+}
+string Element::toString(){
+    stringstream ss;
+    string s, buffer;
+    for(int i = 0; i < t_size ; i ++) {
+        ss << t[i];
+        ss >> buffer;
+        s.append(buffer);
+        if(i < t_size-1) {
+            s.append(" ");
+        }
+    }
+    return s;
+}
+int Element::getRank() {
+    return rank;
+}
+bool Element::getFlag() {
+    return flag;
+}
+void Element::setFlag(bool b) {
+    flag = b;
+}
+double Element::getX(int i) { return x[i]; }
+void Element::print_double(double d){
+    stringstream ss;
+    ss << fixed << setprecision(6) << d;
+    string s = ss.str();
+    s = s.substr(0, s.find_last_not_of('0') + 1);
+
+    if (s.find('.') == (s.size() - 1)) {
+        s = s.substr(0, (s.size() - 1));
+    }
+
+    cout << s;
+}
--- Element.h
+++ Element.h
@@ -1,10 +1,8 @@
-//
-// Created by omril on 01/10/2022.
-//
-
 #ifndef EXERCISE05_ELEMENT_H
 #define EXERCISE05_ELEMENT_H
 
+#include <iostream>
+#include <sstream>
 #include <iomanip>
 
 using namespace std;
@@ -17,62 +15,18 @@ private:
     int x_size;
     int t_size;
     bool flag;
+
 public:
-    Element() {
-        x = NULL;
-        t = NULL;
-        x_size = 0;
-        t_size = 0;
-        rank = 0;
-        flag = false;
-    }
-    Element(double* x, double* t, int x_size, int t_size) {
-        this->x = x;
-        this->t = t;
-        this->x_size = x_size;
-        this->t_size = t_size;
-        rank = 0;
-        flag = false;
-    }
-    void updateRank(int r) {
-        rank = r;
-    }
-    string toString(){
-        stringstream ss;
-        string s, buffer;
-        for(int i = 0; i < t_size ; i ++) {
-            ss << t[i];
-            ss >> buffer;
-            s.append(buffer);
-            if(i < t_size-1) {
-                s.append(" ");
-            }
-        }
-        return s;
-    }
-    int getRank() {
-        return rank;
-    }
-    bool getFlag() {
-        return flag;
-    }
-    void setFlag(bool b) {
-        flag = b;
-    }
-    double getX(int i) { return x[i]; }
-    void print_double(double d){
-        stringstream ss;
-        ss << fixed << setprecision(6) << d;
-        string s = ss.str();
-        s = s.substr(0, s.find_last_not_of('0') + 1);
-
-        if (s.find('.') == (s.size() - 1)) {
-            s = s.substr(0, (s.size() - 1));
-        }
-
-        cout << s;
-    }
+    Element();
+    Element(double* x, double* t, int x_size, int t_size);
+    void updateRank(int r);
+    string toString();
+    int getRank();
+    bool getFlag();
+    void setFlag(bool b);
+    double getX(int i);
+    void print_double(double d);
 };
 
 
-#endif //EXERCISE05_ELEMENT_H
+#endif
